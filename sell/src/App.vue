d07968d07107c0049e73a5bf68ddd0dcaacd5f89
katrinaLHN <haonanl@outlook.com>
2017-04-20 16:51:37 +0800

    creat tab

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,19 +1,39 @@
 <template>
   <div id="app">
-    <div class="header">
-      I am header
-    </div>
+    <!--使用注册以后的组件-->
+    <v-header></v-header>
     <div class="tab">
-      Iam tab
+      <div class="tab-item">商品</div>
+      <div class="tab-item">评论</div>
+      <div class="tab-item">商家</div>
     </div>
     <div class="content">
-      iamcontent
+      i am content
     </div>
   </div>
 </template>
 
 <script>
+  import header from './components/header/header.vue';
+  export default{
+    components: {
+      // 对header进行注册
+      // 因为es6支持简写header相当于header：header
+      'v-header': header
+    }
+  };
+
 </script>
 
-<style>
+<style lang="stylus" rel="stylesheet/stylus">
+  #app
+    .tab
+      display: flex
+      width: 100%
+      justify-content: space-around
+      height: 40px
+      line-height: 40px
+      .tab-item
+        flex: 1
+        text-align: center
 </style>
--- sell/src/components/header/header.vue
+++ sell/src/components/header/header.vue
@@ -1,9 +1,11 @@
 <template>
-
+  <div class="header">
+    握手 header
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-
+  export default{};
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">

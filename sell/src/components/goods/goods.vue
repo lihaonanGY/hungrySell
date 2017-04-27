253d5d3fb9cf4f22a0cb30764c685bd9b1e74870
katrinaLHN <haonanl@outlook.com>
2017-04-27 11:27:07 +0800

    goods data

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,10 +1,48 @@
 <template>
-  <div>i am goods</div>
+  <div class="goods">
+    <div class="menu-wrapper"></div>
+    <div class="foods-wrapper"></div>
+  </div>
 </template>
 
 <script type="text/ecmascript-6">
-  export default {};
+  const ERR_OK = 0;
+  export default {
+    data() {
+      return {
+        goods: {}
+      };
+    },
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    created() {
+      this.$http.get('/api/goods').then((response) => {
+        response = response.body;
+        if (response.errNo === ERR_OK) {
+          this.goods = response.data;
+          console.log(this.goods);
+        }
+      });
+    }
+  };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  .goods
+    display flex
+    width 100%
+    position absolute
+    top 174px
+    bottom 46px
+    overflow hidden
+    .menu-wrapper
+      flex 0 0 80px
+      /*width是为了兼容安卓设备*/
+      width 80px
+      background #f3f5f7
+    .foods-wrapper
+      flex 1
 </style>

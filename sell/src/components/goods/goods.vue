5aa8d0799a459bc9f298234f88964b5f1efcf7cd
katrinaLHN <haonanl@outlook.com>
2017-04-28 23:32:43 +0800

    header completed

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper"></div>
+    <div class="menu-wrapper">
+      <ul>
+        <li v-for="item in goods" class="menu-item">
+          <span class="text">
+            <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
+          </span>
+        </li>
+      </ul>
+    </div>
     <div class="foods-wrapper"></div>
   </div>
 </template>
@@ -23,14 +31,16 @@
         response = response.body;
         if (response.errNo === ERR_OK) {
           this.goods = response.data;
-          console.log(this.goods);
+//          console.log(this.goods);
         }
       });
+      this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
     }
   };
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import "../../common/stylus/mixin.styl"
   .goods
     display flex
     width 100%
@@ -43,6 +53,31 @@
       /*width是为了兼容安卓设备*/
       width 80px
       background #f3f5f7
+      .menu-item
+        display table
+        height 54px
+        width 56px
+        padding 0 12px
+        line-height 14px
+        font-size 12px
+        color rgb(240 20 20)
+        .icon
+          display inline-block
+          width 18px
+          height 18px
+          /*margin-right 4px*/
+          background-size 18px 18px
+          background-repeat no-repeat
+          &.decrease
+            bg-img('decrease_3')
+          &.discount
+            bg-img('discount_3')
+          &.guarantee
+            bg-img('guarantee_3')
+          &.invoice
+            bg-img('invoice_3')
+          &.special
+            bg-img('special_3')
     .foods-wrapper
       flex 1
 </style>

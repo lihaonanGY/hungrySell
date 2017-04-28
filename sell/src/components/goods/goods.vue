5b8169fef4a194022625b5bac1e9a2db692db6a1
katrinaLHN <haonanl@outlook.com>
2017-04-29 01:45:26 +0800

    第一次半夜push

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -3,13 +3,38 @@
     <div class="menu-wrapper">
       <ul>
         <li v-for="item in goods" class="menu-item">
-          <span class="text">
+          <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper">
+      <ul>
+        <li v-for="item in goods" class="food-list">
+          <h1 class="title">{{item.name}}</h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-1px">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon">
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
@@ -53,20 +78,19 @@
       /*width是为了兼容安卓设备*/
       width 80px
       background #f3f5f7
+      font-size 0
       .menu-item
         display table
         height 54px
         width 56px
         padding 0 12px
         line-height 14px
-        font-size 12px
-        color rgb(240 20 20)
         .icon
           display inline-block
-          width 18px
-          height 18px
-          /*margin-right 4px*/
-          background-size 18px 18px
+          width 12px
+          height 12px
+          margin-right 2px
+          background-size 12px 12px
           background-repeat no-repeat
           &.decrease
             bg-img('decrease_3')
@@ -78,6 +102,61 @@
             bg-img('invoice_3')
           &.special
             bg-img('special_3')
+        .text
+          display table-cell
+          vertical-align middle
+          width 56px
+          border-1px(rgba(7,17,27,0.1))
+          font-size 12px
     .foods-wrapper
       flex 1
+      .title
+        padding-left 14px
+        height 26px
+        line-height 26px
+        border-left 2px solid #d9dde1
+        font-size 12px
+        color rgb(147,153,159)
+        background #f3f5f7
+      .food-item
+        display flex
+        margin 18px
+        padding-bottom 18px
+        border-1px(rgba(7,17,27,0.1))
+        &:last-child
+          border-none()
+          margin-bottom 0
+        .icon
+          flex 0 0 57px
+          margin-right 10px
+        .content
+          flex 1
+          .name
+            margin 2px 0 8px 0
+            font-size 14px
+            line-height 14px
+            height 14px
+            color rgb(7,17,27)
+          .desc, .extra
+            font-size 10px
+            line-height 10px
+            color rgb(147,153,159)
+          .desc
+            margin-bottom 8px
+          .extra
+            .count
+              margin-right 12px
+          .price
+            font-weight 700
+            line-height 24px
+            .now
+              margin-right 8px
+              font-size 14px
+              color rgb(240,20,20)
+            .old
+              text-decoration line-through
+              font-size 10px
+              color rgb(147,153,159)
+
+
 </style>

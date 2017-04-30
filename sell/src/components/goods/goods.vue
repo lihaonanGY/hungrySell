3af83b284c4c2ae23598fd432ff937138b639ad2
katrinaLHN <haonanl@outlook.com>
2017-05-01 02:04:51 +0800

    goods scroll finished

--- sell/src/components/goods/goods.vue
+++ sell/src/components/goods/goods.vue
@@ -1,17 +1,18 @@
-<template>
+<template xmlns:v-el="http://www.w3.org/1999/xhtml">
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" v-el:menu-wrapper>
       <ul>
-        <li v-for="item in goods" class="menu-item">
+        <li v-for="item in goods" class="menu-item" :class="{'current': currentIndex === $index}"
+            @click="selectMenu($index,$event)">
           <span class="text border-1px">
             <span v-show="item.type>0" class="icon" :class="classMap[item.type]"></span>{{item.name}}
           </span>
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" v-el:foods-wrapper>
       <ul>
-        <li v-for="item in goods" class="food-list">
+        <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
             <li v-for="food in item.foods" class="food-item border-1px">
@@ -22,12 +23,11 @@
                 <h2 class="name">{{food.name}}</h2>
                 <p class="desc">{{food.description}}</p>
                 <div class="extra">
-                  <span class="count">月售{{food.sellCount}}份</span>
-                  <span>好评率{{food.rating}}%</span>
+                  <span class="count">月售{{food.sellCount}}份</span><span>好评率{{food.rating}}%</span>
                 </div>
                 <div class="price">
-                  <span class="now">￥{{food.price}}</span>
-                  <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+                  <span class="now">￥{{food.price}}</span><span v-show="food.oldPrice"
+                                                                class="old">￥{{food.oldPrice}}</span>
                 </div>
               </div>
             </li>
@@ -39,16 +39,32 @@
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll';
+
   const ERR_OK = 0;
   export default {
     data() {
       return {
-        goods: {}
+        goods: {},
+        listHeight: [],
+        scrollY: 0
       };
     },
     props: {
       seller: {
         type: Object
+      }
+    },
+    computed: {
+      currentIndex() {
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i];
+          let height2 = this.listHeight[i + 1];
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i;
+          }
+        }
+        return 0;
       }
     },
     created() {
@@ -57,9 +73,47 @@
         if (response.errNo === ERR_OK) {
           this.goods = response.data;
 //          console.log(this.goods);
+          this.$nextTick(() => {
+            this._initScroll();
+            this._caculateHeight();
+          });
         }
       });
       this.classMap = ['decrease', 'discount', 'special', 'invoice', 'guarantee'];
+    },
+    methods: {
+      selectMenu(index, event) {
+        if (!event._constructed) {
+          return;
+        }
+//        console.log(index);
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let el = foodList[index];
+        this.foodsScroll.scrollToElement(el, 300);
+      },
+      _initScroll() {
+        this.menuScroll = new BScroll(this.$els.menuWrapper, {
+          click: true
+        });
+        this.foodsScroll = new BScroll(this.$els.foodsWrapper, {
+          probeType: 3
+        });
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y));
+//          console.log(this.scrollY);
+        });
+      },
+      _caculateHeight() {
+        let foodList = this.$els.foodsWrapper.getElementsByClassName('food-list-hook');
+        let height = 0;
+        this.listHeight.push(height);
+        for (let i = 0; i < foodList.length; i++) {
+          height += foodList[i].clientHeight;
+          this.listHeight.push(height);
+        }
+        /* 输出计算后的高度区间数组*/
+//        console.log(this.listHeight);
+      }
     }
   };
 </script>
@@ -85,6 +139,14 @@
         width 56px
         padding 0 12px
         line-height 14px
+        &.current
+          position relative
+          margin-top -1px
+          z-index 10
+          background #fff
+          font-weight 700
+          .text
+            border-none()
         .icon
           display inline-block
           width 12px
@@ -106,7 +168,7 @@
           display table-cell
           vertical-align middle
           width 56px
-          border-1px(rgba(7,17,27,0.1))
+          border-1px(rgba(7, 17, 27, 0.1))
           font-size 12px
     .foods-wrapper
       flex 1
@@ -116,13 +178,13 @@
         line-height 26px
         border-left 2px solid #d9dde1
         font-size 12px
-        color rgb(147,153,159)
+        color rgb(147, 153, 159)
         background #f3f5f7
       .food-item
         display flex
         margin 18px
         padding-bottom 18px
-        border-1px(rgba(7,17,27,0.1))
+        border-1px(rgba(7, 17, 27, 0.1))
         &:last-child
           border-none()
           margin-bottom 0
@@ -136,13 +198,14 @@
             font-size 14px
             line-height 14px
             height 14px
-            color rgb(7,17,27)
+            color rgb(7, 17, 27)
           .desc, .extra
             font-size 10px
             line-height 10px
-            color rgb(147,153,159)
+            color rgb(147, 153, 159)
           .desc
             margin-bottom 8px
+            line-height 12px
           .extra
             .count
               margin-right 12px
@@ -152,11 +215,11 @@
             .now
               margin-right 8px
               font-size 14px
-              color rgb(240,20,20)
+              color rgb(240, 20, 20)
             .old
               text-decoration line-through
               font-size 10px
-              color rgb(147,153,159)
+              color rgb(147, 153, 159)
 
 
 </style>

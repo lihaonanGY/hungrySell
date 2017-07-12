b6bb637f4cd0cd58181980ffc7b45cf5d3828816
katrinaLHN <haonanl@outlook.com>
2017-07-12 16:34:47 +0800

    good-details

---
+++ src/components/food/food.vue
@@ -0,0 +1,112 @@
+<template>
+  <div v-show="showFlag" class="food" transition="move">
+    <div class="food-content">
+      <div class="image-header">
+        <img :src="food.image">
+        <div class="back" @click="hide">
+          <i class="icon-arrow_lift"></i>
+        </div>
+      </div>
+      <div class="content">
+        <h1 class="title">{{food.name}}</h1>
+        <div class="detail">
+          <span class="sell-count">月售{{food.sellCount}}份</span>
+          <span class="rating">好评率{{food.raing}}份</span>
+        </div>
+        <div class="price">
+          <span class="now">￥{{food.price}}</span>
+          <span v-show="food.oldPrice" class="old">￥{{food.oldPrice}}</span>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: {
+        type: Object
+      }
+    },
+    data() {
+      return {
+        showFlag: false
+      };
+    },
+    methods: {
+      show() {
+        this.showFlag = true;
+      },
+      hide() {
+        this.showFlag = false;
+      }
+    }
+  };
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position fixed
+    left 0
+    top 0
+    bottom 48px
+    z-index 30
+    background #fff
+    width 100%
+    &.move-transition
+      transition all 0.2s linear
+      transform translate3d(0, 0, 0)
+    &.move-enter, &.move-leave
+      transform translate3d(100%, 0, 0)
+    .image-header
+      position relative
+      width 100%
+      height 0
+      padding-top 100%
+      img
+        position absolute
+        width 100%
+        height: 100%
+        left 0
+        top 0
+      .back
+        position absolute
+        left 0
+        top 10px
+        .icon-arrow_lift
+          display block
+          padding 10px
+          font-size 20px
+          color #fff
+    .content
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 8px
+        font-size 14px
+        font-weight 700
+        color rgb(7, 17, 27)
+      .detail
+        margin-bottom 18px
+        height 10px
+        line-height 10px
+        font-size 0
+        .sell-count, .rating
+          font-size 10px
+          color rgb(147, 153, 159)
+        .sell-count
+          margin-right 12px
+
+      .price
+        font-weight 700
+        line-height 24px
+        .now
+          margin-right 8px
+          font-size 14px
+          color rgb(240, 20, 20)
+        .old
+          text-decoration line-through
+          font-size 10px
+          color rgb(147, 153, 159)
+</style>
--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -15,7 +15,7 @@
         <li v-for="item in goods" class="food-list food-list-hook">
           <h1 class="title">{{item.name}}</h1>
           <ul>
-            <li v-for="food in item.foods" class="food-item border-1px">
+            <li @click="selectFood(food,$event)" v-for="food in item.foods" class="food-item border-1px">
               <div class="icon">
                 <img width="57" height="57" :src="food.icon">
               </div>
@@ -43,12 +43,14 @@
     <shopcart v-ref:shopcart :select-foods="selectFoods" :delivery-price="seller.deliveryPrice"
               :min-price="seller.minPrice"></shopcart>
   </div>
+  <food :food="selectedFood" v-ref:food></food>
 </template>
 
 <script type="text/ecmascript-6">
   import BScroll from 'better-scroll';
   import shopcart from 'components/shopcart/shopcart';
   import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import food from 'components/food/food';
 
   const ERR_OK = 0;
 
@@ -57,7 +59,8 @@
       return {
         goods: {},
         listHeight: [],
-        scrollY: 0
+        scrollY: 0,
+        selectedFood: {}
       };
     },
     props: {
@@ -115,6 +118,13 @@
         let el = foodList[index];
         this.foodsScroll.scrollToElement(el, 300);
       },
+      selectFood(food, event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.selectedFood = food;
+        this.$refs.food.show();
+      },
       _drop(target) {
 //        体验优化，异步执行下落动画
         this.$nextTick(() => {
@@ -148,7 +158,8 @@
     },
     components: {
       shopcart,
-      cartcontrol
+      cartcontrol,
+      food
     },
     events: {
       'cart.add'(target) {
